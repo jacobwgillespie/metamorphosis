de877b4ba7fd340a08e21b8342444634393cc7ff
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-01-02 11:06:22 -0600

    Add RefCountedFuture

---
+++ src/RefCountedFuture.ts
@@ -0,0 +1,35 @@
+import {Future} from './Future'
+
+const voidFn = () => {}
+
+export class RefCountedFuture<T> extends Future<T> {
+  private _refs: Future<void>[] = []
+
+  complete(valueOrError: T | Error) {
+    super.complete(valueOrError)
+    return Promise.all(this._refs).then(voidFn)
+  }
+
+  completeWith(other: PromiseLike<T>) {
+    return super.completeWith(other).then(voidFn, voidFn)
+  }
+
+  success(value: T) {
+    super.success(value)
+    return Promise.all(this._refs).then(voidFn)
+  }
+
+  failure(error: any) {
+    super.failure(error)
+    return Promise.all(this._refs).then(voidFn)
+  }
+
+  wait() {
+    const ref = new Future<void>()
+    this._refs.push(ref)
+    return this.then(val => {
+      ref.success()
+      return val
+    })
+  }
+}
