51f1726a85df329a14228eca720e07a2cc647679
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-01-02 12:57:58 -0600

    Update Netlify URL

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Metamorphosis', // Title for your website.
   tagline: 'Async Stream Processing for TypeScript',
-  url: 'https://metamorphosis.netlify.com', // Your website URL
+  url: 'https://metamorphosis-js.netlify.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
