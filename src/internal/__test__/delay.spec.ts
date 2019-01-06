5ab0b4f8c0959060dd4a1831b5f1ecc724064623
Jacob Gillespie <jacobwgillespie@gmail.com>
2019-01-06 11:38:56 -0600

    Tolerate fast tests when testing delays

--- src/internal/__test__/delay.spec.ts
+++ src/internal/__test__/delay.spec.ts
@@ -5,5 +5,5 @@ import {toArray} from '../toArray'
 test('delay', async () => {
   const start = Date.now()
   await toArray(delay(of(1, 2, 3), 30))
-  expect(Date.now() - start).toBeGreaterThanOrEqual(30)
+  expect(Date.now() - start).toBeGreaterThanOrEqual(20)
 })
--- src/internal/__test__/delayEach.spec.ts
+++ src/internal/__test__/delayEach.spec.ts
@@ -5,5 +5,5 @@ import {toArray} from '../toArray'
 test('delayEach', async () => {
   const start = Date.now()
   await toArray(delayEach(of(1, 2, 3), 10))
-  expect(Date.now() - start).toBeGreaterThanOrEqual(30)
+  expect(Date.now() - start).toBeGreaterThanOrEqual(20)
 })
